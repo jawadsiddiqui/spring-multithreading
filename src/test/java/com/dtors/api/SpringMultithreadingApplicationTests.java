247ec3f3e4f4867b97c7fe955abe91ef19c3c2e4
Jawad Siddiqui <jsiddiqui@wsgc.com>
2023-01-19 09:10:20 +0500

    Initial commit

---
+++ src/test/java/com/dtors/api/SpringMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtors.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
